bdfaca9ab097f62e25c0cc72915de08736c94628
Ilija Gutic <capigutic123@gmail.com>
2022-12-07 22:50:56 +0100

    GallerySeeder

---
+++ database/factories/GalleryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Gallery>
+ */
+class GalleryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(80),
+            'user_id' => User::inRandomOrder()->first()->id,
+        ];
+    }
+}
